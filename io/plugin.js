cff00ee19f83dbce418039b9d8a3ff1acb3b2803
Richard Schloss <richardeschloss@gmail.com>
2019-10-04 17:39:22 -0700

    Minor bug fixes.

--- io/plugin.js
+++ io/plugin.js
@@ -2,20 +2,29 @@ import io from 'socket.io-client'
 import consola from 'consola'
 
 function nuxtSocket(ioOpts) {
-  const { name, channel, ...connectOpts } = ioOpts
+  const { name, channel = '', ...connectOpts } = ioOpts
   const { sockets } = <%= JSON.stringify(options) %>
-  let useSocket;
+
+  if( !sockets || sockets.length == 0 ){
+    throw new Error("Please configure sockets if planning to use nuxt-socket-io: \r\n [{name: '', url: ''}]")
+    return;
+  }
+  let useSocket = null;
   if (sockets && sockets.length && sockets.length > 0) {
     if (!name) {
-      useSocket = sockets.find((s) => s.dflt === true )
-      if (!useSocket) {
-        useSocket = sockets[0]
-      }
+      useSocket = sockets.find((s) => s.default === true)
+    } else {
+      useSocket = sockets.find((s) => s.name == name)
     }
+  }
+
+  if (!useSocket) {
+    useSocket = sockets[0]
   }
 
   if( !useSocket.url ){
     throw new Error('URL must be defined for nuxtSocket')
+    return;
   }
 
   useSocket.url += channel
