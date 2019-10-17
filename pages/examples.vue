98171511b62d383182916ea6c57ddad7d301e341
Richard Schloss <richard.e.schloss@gmail.com>
2019-10-16 23:45:31 -0700

    Removed unused arg

--- pages/examples.vue
+++ pages/examples.vue
@@ -57,7 +57,7 @@ export default {
   computed: mapState({
     progressVuex: (state) => state.examples.progress
   }),
-  mounted(ctx) {
+  mounted() {
     this.socket = this.$nuxtSocket({
       channel: '/examples'
     })
