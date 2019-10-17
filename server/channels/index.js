0cf0f2af03cb84e377ecb993bc55f142b98fb15f
Richard Schloss <richard.e.schloss@gmail.com>
2019-10-16 20:08:21 -0700

    Updated index to consume vuex chat messages

--- server/channels/index.js
+++ server/channels/index.js
@@ -1,8 +1,19 @@
 function Svc() {
   return Object.freeze({
-    getMessage: (msg) => {
+    getMessage: ({ notify, ...data }) => {
+      const msgs = [
+        'Hi, this is a chat message from IO server!',
+        'Hi, this is another chat message from IO server!'
+      ]
+      let msgIdx = 0
+      const timer = setInterval(() => {
+        notify({ evt: 'chatMessage', data: msgs[msgIdx] })
+        if (++msgIdx >= msgs.length) {
+          clearInterval(timer)
+        }
+      }, 500)
       return new Promise((resolve, reject) => {
-        resolve('It worked! Received msg: ' + JSON.stringify(msg))
+        resolve('It worked! Received msg: ' + JSON.stringify(data))
       })
     }
   })
