a2ec39087c0b97febf64b873ecce4220b9d6b45c
Richard Schloss <richard.e.schloss@gmail.com>
2020-03-21 02:00:13 -0700

    Simplified api structure, I think

--- server/namespaces/dynamic.js
+++ server/namespaces/dynamic.js
@@ -1,12 +1,4 @@
 const Schemas = {
-  Person: {
-    firstName: String,
-    lastName: String
-  },
-  Transaction: {
-    date: Date,
-    amt: Number
-  },
   Item: {
     id: '',
     item: '',
@@ -14,51 +6,79 @@ const Schemas = {
   }
 }
 
-function API() {
-  const version = 1.01
+const { Item } = Schemas
 
-  const schemas = Object.freeze({
+const api = {
+  version: 1.02,
+  nodeType: 'server',
+  evts: {
+    msgRxd: {},
+    progress: {
+      evt: '',
+      val: 0
+    }
+  },
+  methods: {
     getItems: {
       evts: {
         progress: 0
       },
-      resp: []
+      resp: [Schemas.Item]
     },
     getItem: {
-      msgIn: {
+      msg: {
         id: ''
       },
       resp: Schemas.Item
     }
-  })
+  }
+}
 
-  const methods = Object.freeze({
-    getItems({ notify, ...data }) {
-      console.log('data!', data)
+function Svc(socket) {
+  return Object.freeze({
+    getAPI: () => Promise.resolve(api),
+
+    /* Methods */
+    getItems({ notify }) {
       notify({
         evt: 'progress',
         data: 0.32
       })
-      return Promise.resolve([{ id: 'item1' }, { id: 'item2' }])
+      const items = [
+        {
+          id: 'item1',
+          name: 'Some Item (1)',
+          desc: 'Some description (1)'
+        },
+        {
+          id: 'item2',
+          name: 'Some Item (2)',
+          desc: 'Some description (2)'
+        }
+      ]
+      return Promise.resolve(items)
     },
-    getItem({ notify, ...data }) {
-      console.log('received msg', data)
-      return Promise.resolve({ msg: 'ok2' })
-    }
-  })
-
-  return Object.freeze({
-    ...methods,
-    api() {
-      return Promise.resolve({
-        methods: Object.keys(methods),
-        schemas,
-        version
+    getItem({ notify, id }) {
+      console.log('received msg', id)
+      notify({
+        evt: 'msgRxd',
+        data: {
+          id
+        }
       })
+      const ItemOut = Object.assign(
+        { ...Item },
+        {
+          id,
+          name: 'Some Item',
+          desc: 'Some description'
+        }
+      )
+      return Promise.resolve(ItemOut)
     }
   })
 }
 
 module.exports = {
-  Svc: API
+  Svc
 }
