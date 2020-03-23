286d0093ffb701da3c5d543022845eddcefccafe
Richard Schloss <richard.e.schloss@gmail.com>
2020-03-23 00:28:36 -0700

    Cleaned up UI a bit.

--- pages/ioApi.vue
+++ pages/ioApi.vue
@@ -2,50 +2,28 @@
   <div class="container">
     <h2 id="hdr">Dynamic API Examples</h2>
     <h3>Server API Examples</h3>
-    <div style="border: 1px solid; width:100%;">
+    <div v-if="ioApi.ready" style="border: 1px solid; width:100%;">
       <span style="width: 40%; display:inline-block;">
         <label>IO server API (for "home" socket on "/dynamic" channel)</label>
-        <div
-          style="white-space:pre-wrap;"
-          v-text="JSON.stringify(ioApis.home, null, '\t')"
-        ></div>
+        <div style="white-space:pre-wrap;" v-text="ioApi"></div>
       </span>
       <span style="width: 50%; display:inline-block; vertical-align:top;">
         <label>Actions Pane</label>
         <br />
-        <button @click="dynamic.getItems('stuff')">Get Items</button>
+        <button @click="ioApi.getItems()">Get Items</button>
         <br />
         Items:
-        <div
-          style="white-space:pre-wrap;"
-          v-text="JSON.stringify(items, null, '\t')"
-        ></div>
+        <div style="white-space:pre-wrap;" v-text="ioData.getItems.resp" />
+        <br />
+        <input v-model="ioData.getItem.msg.id" placeholder="Item id" />
+        <button @click="ioApi.getItem()">Get Item</button>
+        <div style="white-space:pre-wrap;" v-text="ioData.getItem.resp" />
       </span>
-    </div>
-    <button @click="ioApi.getItem()">Get Item 1</button>
-    <div v-if="ioData.getItems">
-      (Progress: {{ ioData.getItems.progress }})
-      <label>Items: </label>
-      <div v-for="item in ioData.getItems.resp" :key="item.id">
-        {{ item.id }}
-        <ul v-for="(v, k) in item" :key="k">
-          <li>
-            {{ k }}:
-            {{ v }}
-          </li>
-        </ul>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-const Item = {
-  id: '',
-  item: '',
-  desc: ''
-}
-
 const ChatMessage = {
   date: new Date(),
   from: '',
@@ -53,57 +31,18 @@ const ChatMessage = {
   text: ''
 }
 
-function emit(evt, msg, callback) {
-  const Items = [
-    {
-      id: 'item1',
-      name: 'Some Item (1)',
-      desc: 'Some description (1)'
-    },
-    {
-      id: 'item2',
-      name: 'Some Item (2)',
-      desc: 'Some description (2)'
-    }
-  ]
-  const evts = {
-    getItems() {
-      callback(Items)
-    }
-  }
-  console.log('[dummy] emit evt rxd', evt, msg)
-  if (evts[evt]) {
-    evts[evt]()
-  }
-}
-
 export default {
   data() {
     return {
-      ioApis: {},
       ioApi: {},
-      ioData: {}, // TBD
-      ioMsg: {}, // TBD
-      ioResp: {}
+      ioData: {}
     }
   },
-  computed: {
-    dynamic() {
-      return this.ioApi.home['/dynamic']
-    },
-    items() {
-      return this.ioResp.home ? this.ioResp.home.getItems : []
-    },
-    progress() {
-      // this.ioData.getItems.msg // this.ioData.getItems.resp // this.ioData.getItems.progress
-      // this.ioData.progress this.ioData.msgRxd
-      // this.ioData.progress --> { }
-      return this.ioData.getItems ? this.ioData.getItems.progress : 0
-    }
-  },
+  computed: {},
   mounted() {
     this.socket = this.$nuxtSocket({
       channel: '/dynamic',
+      apiVersion: 'latest',
       clientAPI: {
         version: 1.31,
         nodeType: 'client',
@@ -123,70 +62,6 @@ export default {
         }
       }
     })
-    // Rx api:
-    this.ioApis = {
-      home: {
-        // server 1
-        '/dynamic': {
-          version: 1.02,
-          evts: {
-            msgRxd: {},
-            progress: 0
-          },
-          methods: {
-            // Auto-generate these...
-            getItems: {
-              evts: {
-                progress: 0
-              },
-              resp: [Item]
-            },
-            getItem: {
-              msg: {
-                id: ''
-              },
-              resp: Item
-            }
-          }
-        }
-      },
-      server2: {
-        version: 1.31,
-        methods: {
-          getItems: {
-            msg: {},
-            resp: {}
-          }
-        }
-      }
-    }
-
-    // Assemble API for this page:
-    const ctx = this
-    // Init data:
-    ctx.$set(ctx.ioResp, 'home', {}) // Init for each server
-
-    this.ioApi = {
-      home: {
-        '/dynamic': {
-          getItems(msg) {
-            console.log('[ioApi] getItems!', msg)
-            return new Promise((resolve) => {
-              emit('getItems', msg, (resp) => {
-                console.log('resp', resp)
-                ctx.$set(ctx.ioResp.home, 'getItems', resp)
-                resolve(resp)
-              })
-            })
-          }
-        }
-      }
-    }
-  },
-  methods: {
-    getItems() {
-      console.log('getItems')
-    }
   }
 }
 </script>
