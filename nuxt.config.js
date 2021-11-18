7c69490f15e448d00ba4f5913f1ea0991761e3fb
Richard Schloss <richard.e.schloss@gmail.com>
2021-11-18 01:46:32 -0500

    Specified server cfg

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  server: {
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    port: 3000
+  },
   telemetry: false,
   components: true,
   publicRuntimeConfig: {
