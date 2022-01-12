a0d0260863c3502d9a0900fec430b9ca6f153922
Richard Schloss <richard.e.schloss@gmail.com>
2022-01-12 01:20:27 -0500

    Started to update the plugin tests

--- test/Plugin.spec.js
+++ test/Plugin.spec.js
@@ -1,10 +1,9 @@
 import ava from 'ava'
 import { delay } from 'les-utils/utils/promise.js'
 import { register } from '../lib/module.js'
-// eslint-disable-next-line no-unused-vars
-import Plugin from '../lib/plugin.js' // It actually is used by the mock defineNuxtPlugin
-import * as indexStore from '../store/io.js'
-import * as examplesStore from '../store/examples.js'
+import { useNuxtSocket } from '../lib/plugin.js' // It actually is used by the mock defineNuxtPlugin
+// import * as indexStore from '../store/io.js'
+// import * as examplesStore from '../store/examples.js'
 import { pluginCtx } from './utils/plugin.js'
 
 const { serial: test, before, after } = ava
@@ -118,17 +117,21 @@ after(() => {
   ioServerObj.server.close()
 })
 
-test('nuxtSocket injected', (t) => {
+test('nuxtSocket, ioState injected', (t) => {
   t.truthy(ctx.$nuxtSocket)
+  t.truthy(ctx.$ioState)
 })
 
-test('Store selection (if $store undefined)', (t) => {
-  delete ctx.$store
-  ctx.$config.io = {
-    sockets: [{ url: 'http://someUrl' }]
-  }
-  ctx.$nuxtSocket({ warnings: false, info: false })
-  t.truthy(ctx.store.state.$nuxtSocket)
+test('ioState (state for the new "iox")', (t) => {
+  const state = ctx.$ioState().value
+  state.xyz = 123
+  const next = ctx.$ioState().value
+  t.is(next.xyz, 123)
+})
+
+test('useNuxtSocket (nuxtSocket internal state)', (t) => {
+  const state = useNuxtSocket().value
+  t.truthy(state.emitTimeouts)
 })
 
 test('Socket plugin (runtime IO $config defined, sockets undef)', (t) => {
@@ -254,122 +257,6 @@ test('Socket Persistence (persist = true, persisted socket disconnected)', async
   const s2 = ctx.$nuxtSocket({ persist: true, teardown: false })
   await waitForSocket(s2)
   t.not(s1.id, s2.id)
-})
-
-test('Vuex module', async (t) => {
-  const ctx = pluginCtx()
-  ctx.$config = {
-    nuxtSocketIO: {
-      sockets: [
-        {
-          name: 'home',
-          url: 'http://localhost:3000'
-        }
-      ]
-    }
-  }
-  const s = ctx.$nuxtSocket({
-    channel: '/dynamic',
-    persist: true,
-    emitTimeout: 3000
-  })
-  await waitForSocket(s)
-  t.truthy(ctx.$store.state.$nuxtSocket)
-  const state = ctx.$store.state.$nuxtSocket
-  const commit = ctx.$store.commit
-  commit('$nuxtSocket/SET_API', {
-    label: 'home/dynamic',
-    api: {
-      version: '1.2.3'
-    }
-  })
-  commit('$nuxtSocket/SET_CLIENT_API', {
-    label: 'home/dynamic',
-    version: '2.2.1'
-  })
-  t.is(state.ioApis['home/dynamic'].version, '1.2.3')
-  t.is(state.clientApis['home/dynamic'].version, '2.2.1')
-
-  t.is(state.emitTimeouts['home/dynamic'], 3000)
-  await ctx.$store.dispatch('$nuxtSocket/emit', {
-    evt: 'getAPI'
-  })
-    .catch((err) => {
-      // Error occurs because this line fails (on purpose here):
-      // const _socket = socket || _sockets[label]
-      // (persist was set to true instead of a label)
-      t.is(
-        err.message,
-        'socket instance required. Please provide a valid socket label or socket instance'
-      )
-    })
-
-  await ctx.$store
-    .dispatch('$nuxtSocket/emit', {
-      evt: 'getAPIxyz',
-      label: 'home/dynamic',
-      emitTimeout: 500
-    })
-    .catch((err) => {
-      t.is(err.message, 'emitTimeout')
-    })
-
-  await ctx.$store
-    .dispatch('$nuxtSocket/emit', {
-      evt: 'getAPIxyz',
-      socket: s,
-      emitTimeout: 500
-    })
-    .catch((err) => {
-      const json = JSON.parse(err.message)
-      t.is(json.message, 'emitTimeout')
-    })
-
-  ctx.$store.state.$nuxtSocket.emitErrors['home/dynamic'].badRequest = []
-  await ctx.$store
-    .dispatch('$nuxtSocket/emit', {
-      evt: 'badRequest',
-      socket: s
-    })
-    .catch((err) => {
-      const json = JSON.parse(err.message)
-      t.is(json.message, 'badRequest...Input does not match schema')
-    })
-
-  await ctx.$store.dispatch('$nuxtSocket/emit', {
-    evt: 'badRequest',
-    label: 'home/dynamic'
-  })
-
-  t.truthy(state.emitErrors['home/dynamic'])
-  t.true(state.emitErrors['home/dynamic'].badRequest.length > 0)
-  t.is(
-    state.emitErrors['home/dynamic'].badRequest[0].message,
-    'badRequest...Input does not match schema'
-  )
-
-  const s2 = ctx.$nuxtSocket({ channel: '/' })
-  const r2 = await ctx.$store.dispatch('$nuxtSocket/emit', {
-    evt: 'echo',
-    msg: 'hi',
-    socket: s2
-  })
-  const r3 = await ctx.$store.dispatch('$nuxtSocket/emit', {
-    evt: 'echo',
-    msg: 'hi',
-    socket: s2,
-    noAck: true
-  })
-
-  const r4 = await ctx.$store.dispatch('$nuxtSocket/emit', {
-    evt: 'echoUndef',
-    socket: s2
-  })
-  t.is(r2, 'hi')
-  t.falsy(r3)
-  t.falsy(r4)
-  s.close()
-  s2.close()
 })
 
 test('Namespace config (registration)', async (t) => {
@@ -630,114 +517,7 @@ test('Namespace config (emitBacks)', async (t) => {
   s.close()
 })
 
-test('Vuex Opts', async (t) => {
-  const ctx = pluginCtx()
-  ctx.$config = {
-    nuxtSocketIO: {
-      sockets: [
-        {
-          name: 'main',
-          url: 'http://localhost:3000',
-          vuex: {
-            mutations: {}
-          }
-        }
-      ]
-    }
-  }
-  const store = cleanStore()
-  const state = store.state
-  Object.assign(ctx.$store, store)
-  const newData = { sample: 123 }
-  let watchCnt = 0
-  ctx.$store.watch = (stateCb, dataCb) => {
-    watchCnt++
-    stateCb(state)
-    dataCb(newData)
-  }
-  let preEmit; let postEmit = 0; let preEmitFail
-  Object.assign(ctx, {
-    postEmitHook (args) {
-      postEmit++
-    },
-    preEmitVal (args) {
-      preEmit = true
-    },
-    preEmitValFail () {
-      preEmitFail = true
-      return false
-    }
-  })
-  const s = ctx.$nuxtSocket({})
-  const s2 = ctx.$nuxtSocket({
-    channel: '/examples',
-    vuex: {
-      actions: [
-        'nonExist1] someAction [nonExist2',
-        'pre1] someAction2 --> format2 [post1',
-        'chatMessage --> FORMAT_MESSAGE'
-      ],
-      mutations: ['SET_MESSAGE', 'someMutation --> examples/SET_SAMPLE'],
-      emitBacks: [
-        'noPre] examples/sample [noPost',
-        'sample2 <-- examples/sample2',
-        'preEmit] sample2b <-- examples/sample2b [postAck',
-        'titleFromUser', // defined in store/index.js (for issue #35)
-        'preEmitVal] echoHello <-- examples/hello [postEmitHook',
-        'preEmitValFail] echoHello <-- examples/helloFail [postEmitHook'
-      ]
-    }
-  })
-  await waitForSocket(s2)
-  await waitForEchoBack(s2, ['SET_MESSAGE'])
-  t.is(state.chatMessages, 'abc123')
-  await waitForEchoBack(s2, ['someMutation'])
-  t.is(state.examples.sample, 'abc123')
-  await waitForEchoBack(s2, ['someAction', 'someAction2', 'chatMessage'])
-  t.true(state.action)
-  t.is(state.action2, 'ABC123')
-  t.true(state.chatMessages.length > 'abc123'.length)
-  await delay(1000)
-  t.true(preEmit)
-  t.true(preEmitFail)
-  t.is(postEmit, 1)
-  const s3 = ctx.$nuxtSocket({
-    channel: '/examples',
-    vuex: {
-      actions: [
-        'nonExist1] someAction [nonExist2'
-      ]
-    }
-  })
-  // Check that duplicate listeners weren't registered
-  // Only need to check one...
-  t.false(s3.hasListeners('someAction'))
-  const preDupeCnt = watchCnt
-  try {
-    const s4 = ctx.$nuxtSocket({
-      vuex: {
-        emitBacks: [
-          'examples/sample', // attempt to re-register...
-          'xyz'
-        ]
-      }
-    })
-    s4.close()
-  } catch (e) {
-    t.is(e.message, [
-      '[nuxt-socket-io]: Trying to register emitback xyz failed',
-      'because it is not defined in Vuex.',
-      'Is state set up correctly in your stores folder?'
-    ].join('\n'))
-  }
-  t.is(watchCnt, preDupeCnt + 1)
-
-  s.close()
-  s2.close()
-  s3.close()
-})
-
-test('Teardown', (t) => {
+test.only('Teardown', (t) => {
   const ctx = pluginCtx()
   ctx.$config.nuxtSocketIO = {}
   let componentDestroyCnt = 0
@@ -827,7 +607,7 @@ test('Stubs (composition api support)', async (t) => {
   await Promise.all(p)
 })
 
-test('Dynamic API Feature (Server)', async (t) => {
+test.skip('Dynamic API Feature (Server)', async (t) => {
   const ctx = pluginCtx()
   ctx.$config = {
     nuxtSocketIO: {
@@ -903,7 +683,7 @@ test('Dynamic API Feature (Server)', async (t) => {
   t.true(ctx.ioApi.ready)
 })
 
-test('Dynamic API Feature (Client)', async (t) => {
+test.skip('Dynamic API Feature (Client)', async (t) => {
   const ctx = pluginCtx()
   ctx.$config = {
     nuxtSocketIO: {
--- test/utils/plugin.js
+++ test/utils/plugin.js
@@ -1,59 +1,10 @@
+import { reactive, toRef, isReactive } from 'vue'
 const ctx = {
+  payload: {},
   provide (label, fn) {
     ctx['$' + label] = fn
   },
-  $config: {},
-  $store: {
-    state: {},
-    mutations: {},
-    actions: {},
-    namespaced: {},
-    opts: {},
-    commit (label, data) {
-      const labelParts = label.split('/')
-      if (labelParts.length > 1) {
-        const namespace = labelParts[0]
-        const key = labelParts[1]
-        ctx.$store.mutations[namespace][key](ctx.$store.state[namespace], data)
-      } else {
-        ctx.$store.mutations[label](ctx.$store.state, data)
-      }
-    },
-    dispatch (label, data) {
-      const labelParts = label.split('/')
-      if (labelParts.length > 1) {
-        const namespace = labelParts[0]
-        const key = labelParts[1]
-        const state = ctx.$store.state[namespace]
-        const commit = (label, data) => ctx.$store.commit(
-          `${namespace}/${label}`,
-          data
-        )
-        return ctx.$store.actions[namespace][key]({ state, commit }, data)
-      } else {
-        return ctx.$store.actions[label](ctx.$store, data)
-      }
-    },
-    registerModule (name, cfg, opts) {
-      if (!ctx.$store) {
-        // Restore the store if it were deleted
-        ctx.$store = { ...ctx.store }
-      }
-      Object.entries(cfg).forEach(([key, item]) => {
-        ctx.$store[key][name] = typeof item === 'object'
-          ? { ...item }
-          : item
-      })
-      ctx.$store.opts[name] = { ...opts }
-    }
-  },
-  inject (label, obj) {
-    ctx['$' + label] = obj
-  }
-}
-
-export function setup (nuxt) {
-  Object.assign(ctx, nuxt)
+  $config: {}
 }
 
 // lib/plugin.js will call this...
@@ -64,6 +15,21 @@ export function defineNuxtPlugin (cb) {
 
 // This returns a clean copy of the ctx
 export function pluginCtx () {
-  ctx.store = { ...ctx.$store }
   return { ...ctx }
+}
+
+export function useState (key, init) {
+  const nuxtApp = pluginCtx()
+  if (!nuxtApp.payload.useState) {
+    nuxtApp.payload.useState = {}
+  }
+  if (!isReactive(nuxtApp.payload.useState)) {
+    nuxtApp.payload.useState = reactive(nuxtApp.payload.useState)
+  }
+
+  const state = toRef(nuxtApp.payload.useState, key)
+  if (state.value === undefined && init) {
+    state.value = init()
+  }
+  return state
 }
