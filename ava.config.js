88d1f7ff1203daed8c914790aaca2643b432bc02
Richard Schloss <richard.e.schloss@gmail.com>
2022-01-12 20:54:18 -0500

    Updated ava config file

--- ava.config.js
+++ ava.config.js
@@ -4,7 +4,7 @@ export default {
     'test/Module.spec.js',
     'test/Plugin.spec.js',
     'test/SocketStatus.spec.js'
-    // 'test/Demos.spec.js'
+    // 'test/Demos.spec.js' // For demo code only. To be updated as needed
   ],
   nodeArguments: [
     '--experimental-loader=./test/utils/loaders.js'
