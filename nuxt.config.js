41819d7acef59b69bd6155c7c820c9ab9396f4b5
Richard Schloss <richard.e.schloss@gmail.com>
2022-11-27 21:52:32 -0500

    Cleaned up nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   // vite: {
@@ -38,26 +38,6 @@ export default defineNuxtConfig({
       }
     }
   },
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
   /*
    ** Global CSS
    */
